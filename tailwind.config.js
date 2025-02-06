6fd56c2068c00a783ea4f53a32a708ac1e2152ed
xvr2e7 <xvx_sh@outlook.com>
2025-02-05 16:25:13 -0800

    Migrate from CRA to Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
