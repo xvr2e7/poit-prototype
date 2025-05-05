dd62534486ae809cbea04a4d07e0e324bca64c06
xvr2e7 <xvx_sh@outlook.com>
2025-05-04 21:36:20 -0700

    Add analytics

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import { ThemeProvider } from "./components/shared/AdaptiveBackground";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
